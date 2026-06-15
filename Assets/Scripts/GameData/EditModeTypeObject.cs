1d8f7158c674b440880f1e43d206a65a682c8c52
DESKTOP-JUDRP11\pon <ikawa0509@gmail.com>
2026-06-16 06:52:16 +0900

    エディットシーンのエディットモードデータを追加

---
+++ Assets/Scripts/GameData/EditModeTypeObject.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameData
+{
+    public enum EditModeType { Clay, Paint, Animation, ModelSelect }
+
+    [System.Serializable]
+    public class EditModeTypeObject
+    {
+        [SerializeField] private EditModeType editModeType;
+        [SerializeField] private List<GameObject> editModeObjects;
+        [SerializeField] private List<Canvas> editModeUI;
+
+        public void SetActive(int index)
+        {
+            bool isActive = (int)editModeType == index;
+            foreach (var obj in editModeObjects)
+            {
+                obj.SetActive(isActive);
+            }
+
+            foreach (var ui in editModeUI)
+            {
+                ui.enabled = isActive;
+            }
+        }
+    }
+}
