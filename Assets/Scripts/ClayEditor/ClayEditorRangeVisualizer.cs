0d46bb43ed24ddd343bbfcf7768d15169da3ef8e
DESKTOP-JUDRP11\pon <ikawa0509@gmail.com>
2026-06-16 07:00:45 +0900

    マーチングキューブの有効範囲表示処理を追加

---
+++ Assets/Scripts/ClayEditor/ClayEditorRangeVisualizer.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+namespace ClayEditor
+{
+    /// <summary>
+    /// クレイエディターの有効範囲表示クラス
+    /// </summary>
+    public class ClayEditorRangeVisualizer : MonoBehaviour
+    {
+        public Color wireframeColor = Color.yellow;
+
+        /// <summary>
+        /// マーチングキューブの範囲枠を生成
+        /// </summary>
+        public void RefreshWireframe(int size, float scale)
+        {
+            Transform old = transform.Find("Wireframe_Root");
+            if (old) DestroyImmediate(old.gameObject);
+
+            GameObject wire = new GameObject("Wireframe_Root");
+            wire.transform.SetParent(transform, false);
+            LineRenderer lr = wire.AddComponent<LineRenderer>();
+            lr.useWorldSpace = false; lr.startWidth = lr.endWidth = 0.05f;
+            lr.material = new Material(Shader.Find("Sprites/Default"));
+            lr.startColor = lr.endColor = wireframeColor;
+
+            float s = (size * scale) * 0.5f;
+            Vector3[] p = {
+            new Vector3(-s, -s, -s), new Vector3(s, -s, -s), new Vector3(s, s, -s),
+            new Vector3(-s, s, -s), new Vector3(-s, -s, -s), new Vector3(-s, -s, s),
+            new Vector3(s, -s, s), new Vector3(s, s, s), new Vector3(-s, s, s),
+            new Vector3(-s, -s, s), new Vector3(-s, s, s), new Vector3(-s, s, -s),
+            new Vector3(s, s, -s), new Vector3(s, s, s), new Vector3(s, -s, s), new Vector3(s, -s, -s)
+            };
+            lr.positionCount = p.Length; lr.SetPositions(p);
+        }
+    }
+}
