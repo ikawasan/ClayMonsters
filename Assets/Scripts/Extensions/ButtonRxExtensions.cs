f6f8dde3d21a4495fe528aeff634b873019e766f
DESKTOP-JUDRP11\pon <ikawa0509@gmail.com>
2026-07-14 15:40:55 +0900

    ボタンの効果音再生処理追加

--- Assets/Scripts/Extensions/ButtonRxExtensions.cs
+++ Assets/Scripts/Extensions/ButtonRxExtensions.cs
@@ -1,5 +1,7 @@
+using Audio.Interface;
 using System;
 using R3;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 namespace Extensions
@@ -8,8 +10,54 @@ namespace Extensions
     {
         static readonly TimeSpan DefaultClickInterval = TimeSpan.FromMilliseconds(300);
 
-        public static IDisposable SubscribeOnClick(this Button button, Action onClick)
+        /// <summary>
+        /// UI効果音サービスを設定する
+        /// </summary>
+        /// <param name="uiSoundService">再生サービス</param>
+        public static void ConfigureUiSound(IUiSoundService uiSoundService)
         {
+            ButtonUiSoundFeedback.Configure(uiSoundService);
+        }
+
+        /// <summary>
+        /// ボタンにホバーと押下のUI効果音を付与する
+        /// </summary>
+        /// <param name="button">対象ボタン</param>
+        public static void EnsureUiSoundFeedback(this Button button)
+        {
+            if (button == null)
+            {
+                return;
+            }
+
+            button.EnsurePressedVisualReset();
+
+            if (button.GetComponent<ButtonUiSoundFeedback>() != null)
+            {
+                return;
+            }
+
+            button.gameObject.AddComponent<ButtonUiSoundFeedback>();
+        }
+
+        /// <summary>
+        /// 押下中にポインターが外れたとき押下色が残る不具合を防ぐ
+        /// </summary>
+        /// <param name="button">対象ボタン</param>
+        public static void EnsurePressedVisualReset(this Button button)
+        {
+            if (button == null || button.GetComponent<ButtonPressedVisualReset>() != null)
+            {
+                return;
+            }
+
+            button.gameObject.AddComponent<ButtonPressedVisualReset>();
+        }
+
+        public static IDisposable SubscribeOnClick(this Button button, UnityAction onClick)
+        {
+            button.EnsureUiSoundFeedback();
+
             return button.OnClickAsObservable()
                 .Where(_ => button != null)
                 .Where(_ => button.isActiveAndEnabled)
