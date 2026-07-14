f6722231885719426c1931eb86ee6d61ac174317
DESKTOP-JUDRP11\pon <ikawa0509@gmail.com>
2026-07-14 15:42:49 +0900

    オプション設定のセーブ機能追加

--- Assets/Scripts/SaveData/SaveData.cs
+++ Assets/Scripts/SaveData/SaveData.cs
@@ -4,8 +4,23 @@ using System;
 namespace SaveData
 {
     [Serializable]
+    public class VideoOptionSaveData
+    {
+        public bool IsFullScreen = true;
+        public bool VSync = true;
+    }
+
+    [Serializable]
+    public class  SoundOptionSaveData
+    {
+        public float MusicVolume = 0.5f;
+        public float SoundEffectVolume = 0.5f;
+    }
+
+    [Serializable]
     public class SaveData : ISaveData
     {
-
+        public VideoOptionSaveData VideoOptionData = new VideoOptionSaveData();
+        public SoundOptionSaveData SoundOptionData = new SoundOptionSaveData();
     }
 }
