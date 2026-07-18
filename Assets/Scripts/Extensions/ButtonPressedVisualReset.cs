c369331d5eda2b12b7658c0c1af7b2ae53f34ec9
DESKTOP-JUDRP11\pon <ikawa0509@gmail.com>
2026-07-18 15:55:05 +0900

    ボタン上でクリックしたままボタン外へカーソルを移動するとボタンが押されたままになる不具合の修正

--- Assets/Scripts/Extensions/ButtonPressedVisualReset.cs
+++ Assets/Scripts/Extensions/ButtonPressedVisualReset.cs
@@ -1,3 +1,4 @@
+﻿using System.Reflection;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -6,12 +7,20 @@ namespace Extensions
 {
     /// <summary>
     /// 押下中にポインターがボタン外へ出たとき押下色が残る不具合を防ぐ
-    /// PointerExitとPointerUpでSelectableの押下状態を解除する
+    /// PointerExitとPointerUpでSelectableの押下状態を強制解除する
     /// </summary>
     [DisallowMultipleComponent]
     [RequireComponent(typeof(Button))]
     public sealed class ButtonPressedVisualReset : MonoBehaviour, IPointerExitHandler, IPointerUpHandler
     {
+        private static readonly PropertyInfo IsPointerDownProperty = typeof(Selectable).GetProperty(
+            "isPointerDown",
+            BindingFlags.Instance | BindingFlags.NonPublic);
+
+        private static readonly MethodInfo EvaluateAndTransitionMethod = typeof(Selectable).GetMethod(
+            "EvaluateAndTransitionToSelectionState",
+            BindingFlags.Instance | BindingFlags.NonPublic);
+
         private Button button;
 
         private void Awake()
@@ -38,17 +47,39 @@ namespace Extensions
                 return;
             }
 
-            if (eventData.button != PointerEventData.InputButton.Left)
+            if (eventData == null || eventData.button != PointerEventData.InputButton.Left)
             {
                 return;
             }
 
-            if (eventData.pointerPress != button.gameObject)
+            if (eventData.pointerPress != null && eventData.pointerPress != button.gameObject)
             {
                 return;
             }
 
             button.OnPointerUp(eventData);
+            ClearSelectablePointerDown(button);
+            InvokeEvaluateAndTransition(button);
+        }
+
+        private static void ClearSelectablePointerDown(Selectable selectable)
+        {
+            if (selectable == null || IsPointerDownProperty == null)
+            {
+                return;
+            }
+
+            IsPointerDownProperty.SetValue(selectable, false);
+        }
+
+        private static void InvokeEvaluateAndTransition(Selectable selectable)
+        {
+            if (selectable == null || EvaluateAndTransitionMethod == null)
+            {
+                return;
+            }
+
+            EvaluateAndTransitionMethod.Invoke(selectable, null);
         }
     }
 }
