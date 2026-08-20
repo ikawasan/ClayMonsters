f3c1ac05535f67a14000fc290eb611cb5eaad0c7
DESKTOP-JUDRP11\pon <ikawa0509@gmail.com>
2026-08-21 02:05:03 +0900

    SteamWorks.NETを追加

---
+++ Assets/Scripts/Steamworks.NET/SteamAppIds.cs
@@ -0,0 +1,13 @@
+namespace SteamIntegration
+{
+    /// <summary>
+    /// ClayMonstersのSteam AppID定数
+    /// </summary>
+    public static class SteamAppIds
+    {
+        /// <summary>
+        /// ClayMonstersのSteam AppID
+        /// </summary>
+        public const uint ClayMonsters = 5143390u;
+    }
+}
